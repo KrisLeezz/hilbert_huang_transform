a864dac5fed2677990862787cae7abaa82f48817
KrisLeezz <mason_sss@outlook.com>
2019-03-18 20:19:43 +0800

    BEMD完成

--- BEMD_python/BEMD_python/BEMD_python.py
+++ BEMD_python/BEMD_python/BEMD_python.py
@@ -1,6 +1,11 @@
 # -*- coding= utf-8 -*-
 import numpy as np
 import math
+import matlab
+import matlab.engine
+import scipy.io
+import csv
+import matplotlib.pyplot as plt
 ## This program was designed for BEMD, the reference matlab code is from ‘Rehman and Mandic’
 #There are 4 paramters at most:
 #	X,your bivariable data;
@@ -19,65 +24,169 @@ import math
 	#     Workshop on Nonlinear Signal and Image Processing, NSIP-03, Grado (I), June 2003
 	#[3]  N. E. Huang et al., "A confidence limit for the Empirical Mode Decomposition and Hilbert spectral analysis",
 	#     Proceedings of the Royal Society A, Vol. 459, pp. 2317-2345, 2003
+
+global eng
+eng=matlab.engine.start_matlab()
 def memd(x,*args):
-	#参数初始化
-	seq,ndir,N,N_dim,stop_crit,sd,sd2,tol=set_value(x)
+	#参数初始化,产生方向sampling,seq即产生的方向
+	seq,ndir,N,N_dim,stop_crit,sd,sd2,tol,MAXITERATIONS,nbit=set_value(x)
+	print ('-----------------------------------------setvalue success-----------------------------------')
 	#创建x的索引
 	t=np.arange(0,len(x),1)
-	print ('the total num_direction is %d'%ndir)#seq就是生成的方向序列
-	n_imf=1
-	while stop_emd(x,seq,ndir,N_dim)!=0:
-		if stop_crit=='stop':
-			stop_sifting(x,t,sd,sd2,tol,seq,ndir,N,N_dim)
-def stop_sifting(x,t,sd,sd2,tol,seq,ndir,N,N_dim):#停止提取一个IMF循环的停止条件
-	envelope_mean(x,t,seq,ndir,N,N_dim)
-def envelope_mean(x,t,seq,ndir,N,N_dim):
+	if (len(t)!=len(x)):
+		print('the index is not equal to the record')
+	#print ('the total num_direction is %d'%ndir)#seq就是生成的方向序列
+	n_imf=0
+	#判断是否需要进行emd分解
+	q = np.zeros((N_dim,20,N))
+	r=x
+	while stop_emd(r,seq,ndir,N_dim)!=0:
+		print('------------------------the emd should continue----------------------------')
+		m=r
+		#进行IMF的提取
+		#判断是否继续进行IMF的提取，即提取的IMF是否满足条件
+		if stop_crit=='stop':#三个标准
+			stop_sift,env_mean=stop_sifting(m,t,sd,sd2,tol,seq,ndir,N,N_dim)
+		#防止当前imf太小,计算精度达不到
+		if (np.max(abs(m))<np.max(abs(x))):
+			print ('forced stop of EMD : too small amplitude')
+		while stop_sift==1 and nbit<MAXITERATIONS:
+			m=m-env_mean#1087,4
+			if stop_crit=='stop':
+				stop_sift,env_mean=stop_sifting(m,t,sd,sd2,tol,seq,ndir,N,N_dim)
+			nbit=nbit+1
+		m=m.T
+		for v in range(0,N_dim):
+			#m_imf=m[v].reshape((1,-1))
+			if (n_imf<20):
+				q[v][n_imf]=m[v].T.reshape((1,-1))
+			else:print('please add the size of q')
+		#q[n_imf]=m
+		n_imf = n_imf+1
+		r=r-m.T
+		nbit=0
+		
+	q[:,n_imf,:]=r.T#最后一个imf记录r
+	return q
+def stop_sifting(x,t,sd,sd2,tol,seq,ndir,N,N_dim):
+	print('-------------------------------sifting:calculate env_mean----------------------------------')
+	#计算mean
+	env_mean,nem,nzm,amp=envelope_mean(x,t,seq,ndir,N,N_dim)
+	sx= (np.sum(env_mean**2,axis=1))**0.5
+	if (amp.any()!=0):
+		sx=sx.reshape(-1,)
+		sx=sx/amp
+	a=sx>sd
+	aa=np.mean(a)#python的运算精度没有matlab高
+	if (((aa>tol)or any(sx>sd2))and any(nem>2)):
+		stp=1#不满足停止条件
+	else:stp=0
+	#D=x-mean
+	#判断D是否满足IMF条件，是的话IMF=D,return stp_sift=1;否的话R=X-mean，即D，再进行shifting
+	return stp,env_mean
+def envelope_mean(x,t,seq,ndir,N,N_dim):#计算每个方向的包络线
 	#计算包络线的均值和mode振幅的估计
 	NBSYM=2
 	count=0
 	env_mean=np.zeros((len(t),N_dim))
-	amp=np.zeros((len(t),1))
+	amp=np.zeros((len(t),))
 	nem=np.zeros((ndir,1))
-	nzm=np.zerps((ndir,1))
-	for it in range (0,64):
-		print(it)
-		#获取hammersely 序列对应的角度
+	nzm=np.zeros((ndir,1))
+	for it in range (0,ndir):
 		#投影
+		print('the %d direction'%(it+1))
+		y=math.cos(seq[it])*x[:,0]-math.sin(seq[it])*x[:,1]
+		#t=np.arange(0,len(y),1)
 		#计算投影后信号的极值
+		max_pos,max_val,min_pos,min_val,zero=local_peaks(t,y)
+		nem[it]=len(max_pos)+len(min_pos)
+		nzm[it]=len(zero)
 		#边界条件约束
-		#极值插值成廓线
-	#所有方向的阔线取均值
-def stop_emd(x,seq,ndir,N_dim):#判断是否满足stop_emd条件,当提取的极值extream小于三个的时候，停止第一层循环
-	#对于每一个方向，变换hammeresely序列为角度获得值
-	for it in range(0,N_dim):
+		tmin,tmax,zmin,zmax,mode=boundary_conditions(min_pos,max_pos,t,y,x,NBSYM)
+		#极值插值成廓线,三次样条插值
+		if (mode):
+			tmin=np.array(tmin)#savemat那个函数貌似只支持array，matlab出来的函数必须这么转换一下子
+			tmin=tmin.astype(float)
+			tmax=np.array(tmax)
+			tmax=tmax.astype(float)
+			zmin=np.array(zmin)
+			zmin=zmin.astype(float)
+			zmax=np.array(zmax)
+			zmax=zmax.astype(float)
+			tt=np.array(t)
+			tt=tt.astype(float)
+			scipy.io.savemat('tmin.mat',mdict={'tmin':tmin})
+			tminpath='tmin.mat'
+			scipy.io.savemat('tamx.mat',mdict={'tamx':tmax})
+			tmaxpath='tamx.mat'
+			scipy.io.savemat('zmin.mat',mdict={'zmin':zmin})
+			zminpath='zmin.mat'
+			scipy.io.savemat('zmax.mat',mdict={'zmax':zmax})
+			zmaxpath='zmax.mat'
+			scipy.io.savemat('tt.mat',mdict={'t':tt})
+			tpath='tt.mat'
+			#eng=matlab.engine.start_matlab()
+			env_min=eng.myspline(tminpath,zminpath,tpath)#
+			env_min=(np.array(env_min)).T
+			env_max=eng.myspline(tmaxpath,zmaxpath,tpath)#
+			env_max=(np.array(env_max)).T
+			#计算env_mean
+			mean=env_max-env_min
+			mean2=mean**2
+			meansum=np.sum(mean2,axis=1)
+			meansqrt=meansum**0.5
+			amp=amp+meansqrt/2
+			env_mean = env_mean + (env_max+env_min)/2
+			print('wati')
+		else: count=count+1#有的方向可能没有信息了
+	if (ndir>count):#所有方向的阔线取均值
+		env_mean=env_mean/(ndir-count)
+		amp=amp/(ndir-count)
+	else:print('wrong in envelope_mean')
+	return env_mean,nem,nzm,amp
+	
+
+
+def boundary_conditions(indmin,indmax,t,xx,z,nbsym):
+	#这里懒得写调用matlab函数
+	#python的变量不能直接传送到matlab中
+	scipy.io.savemat('indmin.mat',mdict={'indimn':indmin})
+	scipy.io.savemat('indmax.mat',mdict={'indmax':indmax})
+	scipy.io.savemat('t.mat',mdict={'t':t})
+	scipy.io.savemat('x.mat',mdict={'x':xx})
+	scipy.io.savemat('z.mat',mdict={'z':z})
+	scipy.io.savemat('nbsym.mat',mdict={'nbsym':nbsym})
+	#eng=matlab.engine.start_matlab()
+	timn,tmax,zmin,zmax,mode=eng.myboundary_conditions(nargout=5)
+	#eng.exit()
+	return timn,tmax,zmin,zmax,mode
+
+
+def stop_emd(x,seq,ndir,N_dim):#判断是否满足stop_emd条件,当每个方向提取的极值extream小于三个的时候，说明信息不够了，停止进行emd分解
+	#对于每一个方向，根据方向sampling的结果进行投影
+	ner=np.zeros((ndir,1))
+	for it in range(0,ndir):
 		#根据生成的投影方向进行投影
-		#t=np.arange(0,len(x),1)#创造x的位置索引
-		max_pos,max_val,min_pos,min_val,zero=local_peaks(t,x)#寻找极大值和极小值
+		y=math.cos(seq[it])*x[:,0]-math.sin(seq[it])*x[:,1]
+		t=np.arange(0,len(y)+0,1)#创造y的位置索引,用于寻找极值的位置,第一个元素的位置是0
+		max_pos,max_val,min_pos,min_val,zero=local_peaks(t,y)#寻找极大值和极小值，返回索引和值，返回的索引下标是从1开始
 		ner[it]=len(max_pos)+len(min_pos)#记录每个方向极大值和极小值的数目
-	if ner.all()<3:#判断是否停止循环
+	if (ner<3).all():#判断是否停止循环
 		stp=0#循环停止
 	else:stp=1#循环不停止
 	#stp=1#测试条件
 	return stp
 
-def local_peaks(T, X):#
-    """
-	a sampler method, another is to find by parabol, I will add it in the future.
-    Performs extrema detection, where extremum is defined as a point,
-    that is above/below its neighbours.
-
-    See :meth:`EMD.find_extrema`.
-    """
-
-    # Finds indexes of zero-crossings
-    S1, S2 = S[:-1], S[1:]
+def local_peaks(T, S):
+     # 寻找过0的次数
+    S1, S2 = S[:-1], S[1:]#制造计算差分的序列
     indzer = np.nonzero(S1*S2<0)[0]
     if np.any(S==0):
-        iz = np.nonzero(S==0)[0]
-        if np.any(np.diff(iz)==1):
+        iz = (np.nonzero(S==0)[0])
+        if np.any(np.diff(iz)==1):#连续出现两个0,求穿过x轴的点
             zer = (S==0)
-            dz = np.diff(np.append(np.append(0, zer), 0))
-            debz = np.nonzero(dz==1)[0]
+            dz = np.diff(np.append(np.append(0, zer), 0))#前后补0，false是不是0（0），true是1原来的值是0
+            debz = (np.nonzero(dz==1)[0])
             finz = np.nonzero(dz==-1)[0]-1
             indz = np.round((debz+finz)/2.)
         else:
@@ -138,9 +247,9 @@ def local_peaks(T, X):#
     local_min_pos = T[indmin]
     local_min_val = S[indmin]
 
-    return local_max_pos, local_max_val, local_min_pos, local_min_val, indzer
+    return local_max_pos+1, local_max_val, local_min_pos+1, local_min_val, indzer+1
 
-def set_value(x,num_direction=64,stop_criteria='stop',criteria_value=[0.075,0.75,0.075]):
+def set_value(x,num_direction=4,stop_criteria='stop',criteria_value=[0.075,0.75,0.075]):
 	#validate the number of parameter!!!!!!!!!!!!!!!!!!!但是这个好像没有什么作用
 	if (x.any()==None or num_direction==None or stop_criteria==None or criteria_value==None ):
 		print ('you miss some parameter')
@@ -165,9 +274,9 @@ def set_value(x,num_direction=64,stop_criteria='stop',criteria_value=[0.075,0.75
 	else: print ('there is a misktake of stop criteria and stop value')
 	maxiterations=[]#最大的迭代条件
 	
-	prm=[2,3,5,7,11,13,17,19,23,29,31,
-	  37,41,43,47,53,59,61,67,71,73,79,
-	  83,89,97,101,103,107,109,113,127,131,137,139,149]
+	#prm=[2,3,5,7,11,13,17,19,23,29,31,
+	#  37,41,43,47,53,59,61,67,71,73,79,
+	#  83,89,97,101,103,107,109,113,127,131,137,139,149]
 	q=x
 	N=q.shape[0]#数据的记录数
 	N_dim=q.shape[1]#数据的维数
@@ -177,22 +286,44 @@ def set_value(x,num_direction=64,stop_criteria='stop',criteria_value=[0.075,0.75
 		print('The total number of data is ',q.shape[0])
 		print('The dimension is ',q.shape[1])	
 	#Specifies maximum number of channels that can be processed by the code
-	Max_channels=32
+	Max_channels=2
 	nbit=0
 	maxiterations=1000
 	#生成均匀的方向，准备后边投影用
-	#生成Hammersley Sequence,注意这里需要的是64个方向的位置，所以样本数i=64，
-	base=[]#用于变换的基底
-	base.append(-ndir)#hammersley sequence的第一个方向是均匀分布i/N
+	##生成Hammersley Sequence,注意这里需要的是64个方向的位置，所以样本数i=64，
+	#base=[]#用于变换的基底
+	#base.append(-ndir)#hammersley sequence的第一个方向是均匀分布i/N
 	if (N_dim==2):#2维=圆,3维=2 sphere=球，4维=3 sphere,..., n维=n-1 sphere，
-					#但是我不知道hammersley对不同维度的数据处理方式是否一样
+					#但是我不知道hammersley对不同维度的数据处理方式是否一样,这里仅支持2维
 		#调用hammersely函数，return seq()
 		seq=[]
-		interval=2*math.pi/ndir#????用pi表示？应该是默认弧度，这里偷懒了reference-BEMD的方法，按照MEMD应该是hammersely序列
+		interval=2*math.pi/ndir#????用pi表示？应该是默认弧度，这里偷懒了reference-BEMD的方法，按照MEMD应该是hammersely序列，但是2维的h序列也是均匀分布
 		for i in range (0,ndir):
 			seq.append(i*interval)
-	return seq,ndir,N,N_dim,stp_crit,sd,sd2,tol
+	return seq,ndir,N,N_dim,stp_crit,sd,sd2,tol,maxiterations,nbit
+def visula(imf):
+	numRows=len(imf[0])
+	t=np.arange(0,len(imf[0][0])+0,1)
+	numCols=2
 
+	plt.figure(12)
+	n=1
+	for i in range(0,numRows):
+		for ii in range (0,numCols):
+			plt.subplot(numRows,numCols,n)
+			plt.plot(t,imf[ii][i])
+			#plt.subplot(numRows,numCols,nn)
+			n=n+1
+	plt.show()
 if __name__ == '__main__':
-	x=np.array([[1,2],[2,3],[3,4]])
-	memd(x)
+	x=[]
+	csvfile=open('test3.csv','r')
+	csvreader=csv.reader(csvfile)
+	for i in csvreader:
+		x.append(i)
+	x=np.array(x)
+	x=x.astype(float)
+	print('read data successfully')
+	IMF=memd(x)
+	visula(IMF)
+	eng.exit()
---
+++ BEMD_python/BEMD_python/myboundary_conditions.m
@@ -0,0 +1,116 @@
+function [tmin,tmax,zmin,zmax,mode] = myboundary_conditions()
+indmin=load('indmin.mat');
+indmin=struct2table(indmin);
+indmin=table2array(indmin);
+indmax=load('indmax.mat');
+indmax=struct2table(indmax);
+indmax=table2array(indmax);
+t=load('t.mat');
+t=struct2table(t);
+t=table2array(t);
+x=load('x.mat');
+x=struct2table(x);
+x=table2array(x);
+z=load('z.mat');
+z=struct2table(z);
+z=table2array(z);
+nbsym=load('nbsym.mat');
+nbsym=struct2table(nbsym);
+nbsym=table2array(nbsym);
+
+lx = length(x);
+if (length(indmin) + length(indmax) < 3)
+    mode = 0;
+    tmin=NaN;tmax=NaN;zmin=NaN;zmax=NaN;
+    return
+else
+    mode=1; %the projected signal has inadequate extrema
+end
+% boundary conditions for interpolations :
+if indmax(1) < indmin(1)
+    if x(1) > x(indmin(1))
+        lmax = fliplr(indmax(2:min(end,nbsym+1)));
+        lmin = fliplr(indmin(1:min(end,nbsym)));
+        lsym = indmax(1);
+    else
+        lmax = fliplr(indmax(1:min(end,nbsym)));
+        lmin = [fliplr(indmin(1:min(end,nbsym-1))),1];
+        lsym = 1;
+    end
+else
+    
+    if x(1) < x(indmax(1))
+        lmax = fliplr(indmax(1:min(end,nbsym)));
+        lmin = fliplr(indmin(2:min(end,nbsym+1)));
+        lsym = indmin(1);
+    else
+        lmax = [fliplr(indmax(1:min(end,nbsym-1))),1];
+        lmin = fliplr(indmin(1:min(end,nbsym)));
+        lsym = 1;
+    end
+end
+
+if indmax(end) < indmin(end)
+    if x(end) < x(indmax(end))
+        rmax = fliplr(indmax(max(end-nbsym+1,1):end));
+        rmin = fliplr(indmin(max(end-nbsym,1):end-1));
+        rsym = indmin(end);
+    else
+        rmax = [lx,fliplr(indmax(max(end-nbsym+2,1):end))];
+        rmin = fliplr(indmin(max(end-nbsym+1,1):end));
+        rsym = lx;
+    end
+else
+    if x(end) > x(indmin(end))
+        rmax = fliplr(indmax(max(end-nbsym,1):end-1));
+        rmin = fliplr(indmin(max(end-nbsym+1,1):end));
+        rsym = indmax(end);
+    else
+        rmax = fliplr(indmax(max(end-nbsym+1,1):end));
+        rmin = [lx,fliplr(indmin(max(end-nbsym+2,1):end))];
+        rsym = lx;
+    end
+end
+tlmin = 2*t(lsym)-t(lmin);
+tlmax = 2*t(lsym)-t(lmax);
+trmin = 2*t(rsym)-t(rmin);
+trmax = 2*t(rsym)-t(rmax);
+
+% in case symmetrized parts do not extend enough
+if tlmin(1) > t(1) || tlmax(1) > t(1)
+    if lsym == indmax(1)
+        lmax = fliplr(indmax(1:min(end,nbsym)));
+    else
+        lmin = fliplr(indmin(1:min(end,nbsym)));
+    end
+    if lsym == 1
+        error('bug')
+    end
+    lsym = 1;
+    tlmin = 2*t(lsym)-t(lmin);
+    tlmax = 2*t(lsym)-t(lmax);
+end
+
+if trmin(end) < t(lx) || trmax(end) < t(lx)
+    if rsym == indmax(end)
+        rmax = fliplr(indmax(max(end-nbsym+1,1):end));
+    else
+        rmin = fliplr(indmin(max(end-nbsym+1,1):end));
+    end
+    if rsym == lx
+        error('bug')
+    end
+    rsym = lx;
+    trmin = 2*t(rsym)-t(rmin);
+    trmax = 2*t(rsym)-t(rmax);
+end
+zlmax =z(lmax,:);
+zlmin =z(lmin,:);
+zrmax =z(rmax,:);
+zrmin =z(rmin,:);
+
+tmin = [tlmin t(indmin) trmin];
+tmax = [tlmax t(indmax) trmax];
+zmin = [zlmin; z(indmin,:); zrmin];
+zmax = [zlmax; z(indmax,:); zrmax];
+end
